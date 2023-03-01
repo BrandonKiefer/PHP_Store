4c8bd7675930ad1488d0b74b9dfd7b42989879ab
kittenmasque <volaju@gmail.com>
2023-03-01 10:28:14 -0800

    created login controller class file

--- classes/login-contr.classes.php
+++ classes/login-contr.classes.php
@@ -47,7 +47,7 @@ class SignupContr extends Signup {
 
     private function emptyInput() {
         $result;
-        if(empty($this->uid) || empty($this->pwd) || empty($this->pwdRepeat) || empty($this->email)) {
+        if(empty($this->uid) || empty($this->pwd)) {
             $result = false;
         }
         else {
@@ -55,52 +55,6 @@ class SignupContr extends Signup {
         }
         return $result;
     }
-    #regex error handler checking for alphanumeric characters in our uid
-    private function invalidUid() {
-        $result;
-        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid))
-        {
-            $result = false;
-        }
-        else 
-        {
-            $result = true;
-        }
-        return $result;
-    }
 
-    #checking for valid email
-    private function invalidEmail() {
-            $result;
-            if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
-                $result = false;
-            }
-            else {
-                $result = true;
-            }
-            return $result;
-        }
 
-        #checks for matching password
-        private function pwdMatch() {
-            $result;
-            if ($this->pwd !== $this-> pwdRepeat){
-                $result = false;
-            }
-            else {
-                $result = true;
-            }
-            return $result;
-        }
-
-        private function uidTakenCheck() {
-            $result;
-            if (!$this->checkUser($this->uid, $this->email)){
-                $result = false;
-            }
-            else {
-                $result = true;
-            }
-            return $result;
-        }
 }
